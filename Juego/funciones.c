a4bcda39cf31775c90ddd2c33c95fe61568b2323
Enrique <luis.gonzalez.hernandez@estudiantxs.uacm.edu.mx>
2026-09-14 06:41:27 -0600

    Comentarios

--- Juego/funciones.c
+++ Juego/funciones.c
@@ -1,9 +1,9 @@
 int sumarRenglon(int renglon[],int n)
 {
-    int suma = 0;
-    for(int i = 0; i < n; i++)
+    int suma = 0; //empieza en 0
+    for(int i = 0; i < n; i++) // recorre cada numero del renglon
     {
-        suma = suma + renglon[i];
+        suma = suma + renglon[i]; //va sumando uno por uno 
     }
 
     return suma;
--- Juego/juego.c
+++ Juego/juego.c
@@ -1,7 +1,6 @@
 #include "funciones.c"
 #include <stdio.h>
 
-int sumarRenglon(int renglon [],int n);
 
 void pruebas_sumaRenglon();
 
@@ -16,14 +15,14 @@ int main()
 void pruebas_sumaRenglon()
 {
 
-    int cuadro [3][3] = {
+    int cuadro [3][3] = {  //una tabla 3x3
         {2,8,7},
         {6,5,1},
         {4,9,3}
      };
 
     printf("prueba funcion con entrada 10");
-    printf("suma de primer renglon:%d,",sumarRenglon(cuadro[0],3));
+    printf("suma de primer renglon:%d,",sumarRenglon(cuadro[0],3));//suma la primera fila : 2+8+7= 17
 
 }
 
@@ -31,6 +30,6 @@ void pruebas_sumaRenglon()
 #include <stdio.h>
 
 int main() {
-    printf("¡Hola bb! 🎉 ¡Ya funciona!\n");
+    printf("¡Ya funciona!\n");
     return 0;
 }
